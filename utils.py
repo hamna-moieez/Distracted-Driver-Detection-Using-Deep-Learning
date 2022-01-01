1d7ec7b7aa655d564d55c89f71c73b021764a5ed
hamna-moieez <hamna21797@gmail.com>
2022-01-01 20:52:06 +0100

    transfer learning

--- utils.py
+++ utils.py
@@ -14,11 +14,10 @@ def view_image(img_path):
         plt.show()
         
 
-DATASET_PATH = '/Users/hamnamoieez/Desktop/PRDL/Distracted-Driver-Detection-Using-Deep-Learning/imgs/'
-data_path = os.path.join(DATASET_PATH, 'train')
+BASE_PATH = '/Users/hamnamoieez/Desktop/PRDL/Distracted-Driver-Detection-Using-Deep-Learning/'
+data_path = os.path.join(BASE_PATH, 'imgs/train')
 
-
-BATCH_SIZE = 16
+BATCH_SIZE = 8
 IMG_SIZE = (224, 224)
 
 def prepare_data(subset):
@@ -54,3 +53,60 @@ def augment_data(input):
         x = tf.keras.layers.RandomFlip('horizontal')(input)
         data_augmentation = tf.keras.layers.RandomRotation(0.2)(x)
         return data_augmentation
+
+def plot_metrics(history):
+        acc = history.history['accuracy']
+        val_acc = history.history['val_accuracy']
+
+        loss = history.history['loss']
+        val_loss = history.history['val_loss']
+
+        plt.figure(figsize=(8, 8))
+        plt.subplot(2, 1, 1)
+        plt.plot(acc, label='Training Accuracy')
+        plt.plot(val_acc, label='Validation Accuracy')
+        plt.legend(loc='lower right')
+        plt.ylabel('Accuracy')
+        plt.ylim([min(plt.ylim()),1])
+        plt.title('Training and Validation Accuracy')
+
+        plt.subplot(2, 1, 2)
+        plt.plot(loss, label='Training Loss')
+        plt.plot(val_loss, label='Validation Loss')
+        plt.legend(loc='upper right')
+        plt.ylabel('Cross Entropy')
+        plt.ylim([0,1.0])
+        plt.title('Training and Validation Loss')
+        plt.xlabel('epoch')
+        plt.show()
+        plt.savefig('metrics.png')
+        return acc, val_acc, loss, val_loss
+
+def plot_finetuned(history_fine, initial_epochs, acc, val_acc, loss, val_loss):
+        acc += history_fine.history['accuracy']
+        val_acc += history_fine.history['val_accuracy']
+
+        loss += history_fine.history['loss']
+        val_loss += history_fine.history['val_loss']
+        plt.figure(figsize=(8, 8))
+        plt.subplot(2, 1, 1)
+        plt.plot(acc, label='Training Accuracy')
+        plt.plot(val_acc, label='Validation Accuracy')
+        plt.ylim([0.8, 1])
+        plt.plot([initial_epochs-1,initial_epochs-1],
+                plt.ylim(), label='Start Fine Tuning')
+        plt.legend(loc='lower right')
+        plt.title('Training and Validation Accuracy')
+
+        plt.subplot(2, 1, 2)
+        plt.plot(loss, label='Training Loss')
+        plt.plot(val_loss, label='Validation Loss')
+        plt.ylim([0, 1.0])
+        plt.plot([initial_epochs-1,initial_epochs-1],
+                plt.ylim(), label='Start Fine Tuning')
+        plt.legend(loc='upper right')
+        plt.title('Training and Validation Loss')
+        plt.xlabel('epoch')
+        plt.show()
+        plt.savefig('metrics_finetuned.png')
+
