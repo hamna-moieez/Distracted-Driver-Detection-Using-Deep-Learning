d49c9a21cef6db6b2089703db39a88189dd23d5b
hamna-moieez <hamna21797@gmail.com>
2022-01-01 20:52:06 +0100

    transfer learning

--- train.py
+++ train.py
@@ -2,15 +2,15 @@
 import utils
 import model
 import vgg
-import finetune
-
+import transferlearn
+import os
 import tensorflow as tf
 
 NUM_CLASSES = 10
 LEARNING_RATE = 0.001
 NUM_EPOCHS = 20
 INPUT_SHAPE = (224,224,3)
-
+weights_path = os.path.join(utils.BASE_PATH, 'weights/')
 
 def compile_model(model, learning_rate):
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
@@ -26,6 +26,9 @@ def fit_model(model, initial_epochs, training, valid):
                     validation_data=valid)
     return history
 
+def save_weights(model, name):
+    model.save(weights_path+name+".h5")
+
 train_data, val_data = utils.get_data()
 
 '''
@@ -35,11 +38,42 @@ TRAIN CUSTOM MODEL
 # history = fit_model(custom_model, NUM_EPOCHS, train_data, val_data)
 
 '''
-TRAIN KERAS BUILTIN VGG16 WITH FINETUNED TOP
+TRAIN KERAS BUILTIN VGG16 WITH FEATURE EXTRACTION
+
+Only training a few layers on top of the base model. 
+The weights of the pre-trained network were not updated during training.
 '''
-vgg16 = vgg.vgg(INPUT_SHAPE, NUM_CLASSES)
-finetuned_model = compile_model(finetune.finetune(vgg16, INPUT_SHAPE, NUM_CLASSES), LEARNING_RATE)
-fit_model(finetuned_model, NUM_EPOCHS, train_data, val_data)
+vgg16 = vgg.vgg(INPUT_SHAPE, NUM_CLASSES, 'feature-extraction')
+fe_model = compile_model(transferlearn.transfer_learn(vgg16, INPUT_SHAPE, NUM_CLASSES), LEARNING_RATE)
+history = fit_model(fe_model, NUM_EPOCHS, train_data, val_data)
+save_weights(fe_model, 'featureExtractor')
+acc, val_acc, loss, val_loss = utils.plot_metrics(history)
+
+
+
+'''
+TRAIN KERAS BUILTIN VGG16 WITH FINETUNED LAYERS
+
+One way to increase performance even further is to train (or "fine-tune") 
+the weights of the top layers of the pre-trained model alongside the training 
+of the classifier you added. The training process will force the weights to be 
+tuned from generic feature maps to features associated specifically with the dataset.
+
+Also, fine-tune a small number of top layers rather than the whole base model. 
+In most convolutional networks, the higher up a layer is, the more specialized it is. 
+The first few layers learn simple & generic features that generalize to almost all types of images. 
+As you go higher up, the features are increasingly more specific to the dataset on which the model was trained. 
+The goal of fine-tuning is to adapt these specialized features to work with the new dataset, rather than overwrite the generic learning.
+'''
+
+fine_tune_epochs = 10
+total_epochs =  NUM_EPOCHS + fine_tune_epochs
+vgg16_fine = vgg.vgg(INPUT_SHAPE, NUM_CLASSES, 'fine-tune')
+fine_model = compile_model(transferlearn.transfer_learn(vgg16_fine, INPUT_SHAPE, NUM_CLASSES), LEARNING_RATE)
+fine_history = fit_model(fine_model, total_epochs, train_data, val_data)
+save_weights(fine_model, 'fineTune')
+
+utils.plot_finetuned(fine_history, NUM_EPOCHS, acc, val_acc, loss, val_loss )
 
 #TODO: Transfer learning and fine tuning over other networks
 #TODO: Test on Deep Learning Architectures other than CNN's
--- vgg.py
+++ vgg.py
@@ -5,12 +5,24 @@ from tensorflow.keras.preprocessing import image
 import numpy as np
 import model
 
-def vgg(input_shape, num_classes):
+def vgg(input_shape, num_classes, mode):
     vgg = VGG16(
         include_top=False, weights='imagenet', input_tensor=None,
         input_shape=input_shape, pooling=None, classes=num_classes,
         classifier_activation='softmax'
         )
-    vgg.trainable = False
+    if mode=='feature-extraction':
+        vgg.trainable = False
+    if mode=='fine-tune':
+        vgg.trainable = True
+        # Let's take a look to see how many layers are in the base model
+        print("Number of layers in the base model: ", len(vgg.layers))
+
+        # Fine-tune from this layer onwards
+        fine_tune_at = 10
+
+        # Freeze all the layers before the `fine_tune_at` layer
+        for layer in vgg.layers[:fine_tune_at]:
+            layer.trainable =  False
     print(vgg.summary())
     return vgg
